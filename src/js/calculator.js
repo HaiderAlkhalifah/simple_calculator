c64a670ac8000e0a758c3c7de8b8a33f614afd1d
Haider Alkhalifah <haideralkhalifah@gmail.com>
2022-11-09 20:50:32 +0300

    Add: some functionality

---
+++ src/js/calculator.js
@@ -0,0 +1,66 @@
+const number = document.querySelectorAll("[data-number]");
+const operation = document.querySelectorAll("[data-operation]");
+const deleteNumber = document.querySelector("[data-delete]");
+const clear = document.querySelector("[data-clear]");
+const calculationDisplay = document.getElementById("calculationDisplay");
+const resultDisplay = document.querySelector("[data-resultDisplay]");
+const equal = document.getElementById("div.equal");
+
+class Calculator {
+  constructor(calculationDisplay, resultDisplay) {
+    this.calculationDisplay = calculationDisplay;
+    this.resultDisplay = resultDisplay;
+    this.clear();
+  }
+
+  clear() {
+    console.log("hello");
+    this.calculationDisplay.innerText = "";
+    this.resultDisplay.innerText = "";
+    this.operation = null;
+  }
+
+  delete() {
+    const text = this.calculationDisplay.innerText;
+    this.calculationDisplay.innerText = text.slice(0, text.length - 1);
+  }
+
+  addNumber(number) {
+    this.calculationDisplay.innerText += number;
+  }
+
+  operation(operation) {}
+
+  calculations() {}
+
+  display() {
+    // this.calculationDisplay.innerText = do wtf you want;
+    this.resultDisplay = this.calculations;
+  }
+}
+
+const calculator = new Calculator(calculationDisplay, resultDisplay);
+
+number.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.addNumber(button.id);
+  });
+});
+
+operation.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.addNumber(button.id);
+  });
+});
+
+clear.addEventListener("click", () => {
+  calculator.clear();
+});
+
+deleteNumber.addEventListener("click", () => {
+  calculator.delete();
+});
+
+equal.addEventListener("click", () => {
+  calculator.calculations
+})
