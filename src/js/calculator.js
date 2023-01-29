2d3fb480bdc27fd9ae3022fb3ec4f4c3746cc8a6
Haider Alkhalifah <haideralkhalifah@gmail.com>
2023-01-29 15:35:12 +0300

    Add: calc class

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -6,6 +6,8 @@ const calculationDisplay = document.getElementById("calculationDisplay");
 const resultDisplay = document.querySelector("[data-resultDisplay]");
 const equal = document.getElementById("=");
 
+let opIsClicked = false;
+
 class Calculator {
   constructor(calculationDisplay, resultDisplay, operations) {
     this.calculationDisplay = calculationDisplay;
@@ -13,12 +15,14 @@ class Calculator {
     this.operation = operations;
     this.clear();
   }
+  
 
   clear() {
     console.log("hello");
     this.calculationDisplay.innerText = "";
     this.resultDisplay.innerText = "";
     this.operation = null;
+    opIsClicked = false;
   }
 
   delete() {
@@ -27,43 +31,111 @@ class Calculator {
   }
 
   addNumber(txt) {
-    this.calculationDisplay.innerText += txt;
+    if (this.resultDisplay.innerText != "") {
+      this.clear();
+      this.calculationDisplay.innerText += txt;
+    } else {
+      this.calculationDisplay.innerText += txt;
+    }
   }
 
   oper(op) {
-    const text = this.calculationDisplay.innerText;
-    const lastText = text[text.length - 1];
-    for (var i = 0; i < operations.length; i++) {
-      if (operations[i].id == lastText) {
-        return
-      };
+    if(opIsClicked){
+    return;
     }
-
-    this.calculationDisplay.innerText += op;
+    if (this.resultDisplay.innerText != "") {
+      this.calculationDisplay.innerText = this.resultDisplay.innerText;
+      this.resultDisplay.innerText = "";
+      this.calculationDisplay.innerText += op;
+      opIsClicked = true;
+    } else if(opIsClicked)
+      return;
+    else {
+      this.calculationDisplay.innerText += op;
+      opIsClicked = true;
+    }
   }
-  //TODO fix calculat "import parse"
+
   calculat() {
-    const text = this.calculationDisplay.innerText;
-    const lastText = text[text.length - 1];
+    
 
-    for (var i = 0; i < operations.length; i++) {
-      if (operations[i].id == lastText) return;
+    let text = this.calculationDisplay.innerText;
+    let x;
+    let y;
+    let op = "1";
+    let num1 = "";
+    let num2 = "";
+    let firstNum = true;
+
+    for (let i = 0; i <= text.length - 1; i++) {
+      if (text.charAt(i) == "×") {
+        text = text.replace(text.charAt(i), "*");
+        console.log(text);
+      } else if (text.charAt(i) == "÷") {
+        text = text.replace(text.charAt(i), "/");
+      }
+
+      if (this.isDigit(text.charAt(i)) && firstNum) {
+        num1 += text.charAt(i);
+      } else if (this.isDigit(text.charAt(i))) {
+        num2 += text.charAt(i);
+      } else {
+        op = text.charAt(i);
+        firstNum = false;
+      }
     }
+    console.log(op);
+    x = parseInt(num1);
+    y = parseInt(num2);
+    let result = new Calc().calculat(x, y, op);
 
-    var inner = this.calculationDisplay.innerText;
-    var expression = Parser.parse(inner);
-    for (var i = 0; i <= expression.length; i++) {
-      expression[i] = expression[i].replace("×", "*");
-      expression[i] = expression[i].replace("÷", "/");
-    }
-
-    result = math.eval(expression);
     this.display(result);
+    opIsClicked = false;
   }
+
 
   display(result) {
     // this.calculationDisplay.innerText = do wtf you want;
     this.resultDisplay.innerText = String(result);
+  }
+
+  isDigit = (function () {
+    let re = /^\d$/;
+    return function (c) {
+      return re.test(c);
+    };
+  })();
+}
+
+class Calc {
+  calculat(x, y, op) {
+    if (op == "+") {
+      return String(this.Add(x, y));
+    } else if (op == "-") {
+      return String(this.Minus(x, y));
+    } else if (op == "*") {
+      return String(this.Multi(x, y));
+    } else if (op == "/") {
+      return String(this.Div(x, y));
+    } else {
+      return "op is not valid";
+    }
+  }
+
+  Div(x, y) {
+    return x / y;
+  }
+
+  Multi(x, y) {
+    return x * y;
+  }
+
+  Minus(x, y) {
+    return x - y;
+  }
+
+  Add(x, y) {
+    return x + y;
   }
 }
 
